8a7415410c8a733d344ba9b119badcd565fde3b5
Tejaswini <tejaswinigorrela3@gmail.com>
2025-07-16 11:07:09 +0530

    perimeter of circle

---
+++ perimeter of circle.c
@@ -0,0 +1,10 @@
+//perimeter of circle
+#include<stdio.h>
+void main()
+{
+	float r,perimeter=0;
+	printf("enter r value:");
+	scanf("%f",&r);
+	perimeter=2*3.14*r;
+	printf("perimeter of circle %f",perimeter);
+}
