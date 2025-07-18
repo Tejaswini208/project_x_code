189367a686b2c4a83010b736bbec66f74acca973
Gorrela Teja Swini <tejaswinigorrela3@gmail.com>
2025-07-18 11:01:54 +0530

    grade evaluation

---
+++ grade evaluation.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+	int score;
+	printf("enter the score:");
+	scanf("%d",&score);
+	if(score>90 && score<=100)
+	{
+		printf("A");
+	}
+	else if(score>80 && score<=90)
+	{
+		printf("B");
+	}
+	else if(score>70 && score<=80)
+	{
+		printf("C");
+	}
+	else if(score>60 && score<=70)
+	{
+		printf("D");
+	}
+	else if(score>=0 && score<=60)
+	{
+		printf("fail");
+	}
+	else
+	{
+		printf("invalid");
+	}
+}
